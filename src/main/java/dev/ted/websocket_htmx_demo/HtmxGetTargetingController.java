938174dd3e360b1142b2399c1edf4b89c8e58152
Ted M. Young <tedyoung@gmail.com>
2024-02-20 14:54:29 -0800

    added multiple swap-oob targets for single GET

--- src/main/java/dev/ted/websocket_htmx_demo/HtmxGetTargetingController.java
+++ src/main/java/dev/ted/websocket_htmx_demo/HtmxGetTargetingController.java
@@ -1,7 +1,6 @@
 package dev.ted.websocket_htmx_demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalTime;
@@ -11,7 +10,7 @@ public class HtmxGetTargetingController {
 
     @GetMapping("/replace-div")
     public String replaceDiv() {
-
+        // language=html
         return """
                 <div id="replaced-div">
                     Replaced DIV content from server, generated at %s...
@@ -21,6 +20,7 @@ public class HtmxGetTargetingController {
 
     @GetMapping("/replace-div-target-of-oob")
     public String replaceDivWithOobSwap() {
+        // language=html
         return """
                 <div id="target-of-oob" hx-swap-oob="true">
                     Replaced DIV content from server, generated at %s...
@@ -36,6 +36,23 @@ public class HtmxGetTargetingController {
                     Replaced DIV content from server, generated at %s...
                 </div>
                 ignored text"""
+                .formatted(LocalTime.now());
+    }
+
+
+    @GetMapping("/replace-multiple-targets-of-oob-swap")
+    public String replaceMultipleTargetsOfOobSwap() {
+        return """
+                <h2 id="another-target-h2"
+                    hx-swap-oob="true"
+                    class="font-bold text-xl text-gray-500 bg-white mt-2">
+                    H2 Has Been Replaced
+                </h2>
+                <div id="multiple-targets-of-oob-swap"
+                     class="border-green-300 bg-yellow-50 p-2"
+                     hx-swap-oob="true">
+                    Replaced DIV content target by server, generated at %s...
+                </div>"""
                 .formatted(LocalTime.now());
     }
 
